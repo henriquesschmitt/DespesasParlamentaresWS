5225f0c42740855a802e358831665bf54f754372
Henrique Schmitt <henriquesschmitt@gmail.com>
2016-08-27 22:56:24 -0300

    Criado web service e primeira URL que retorna todas as despesas do banco de dados

---
+++ src/com/despesas/ws/bd/DespesaBD.java
@@ -0,0 +1,21 @@
+package com.despesas.ws.bd;
+
+import java.util.List;
+
+import com.despesas.ws.dominio.Despesa;
+import com.despesas.ws.util.copy.EntityManagerProducer;
+
+public class DespesaBD {
+    
+    EntityManagerProducer emp = new EntityManagerProducer();
+    
+    @SuppressWarnings("unchecked")
+	public List<Despesa> getDespesas(){
+        return emp.create().createQuery("from Despesa").getResultList();
+    }
+    
+    public Despesa buscarPeloDocumento(int codigo) {
+		return emp.create().find(Despesa.class, codigo);
+	}
+    
+}
